d6b338d806e38b35ae0e6ad121104bac553db7a2
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-09-12 16:44:54 -0600

    Ni idea

--- create_docker_container.sh
+++ create_docker_container.sh
@@ -1,8 +1,6 @@
+#! /bin/bash
 docker run -d \
 --name bo-container \
-# Expongo el puerto 8080 del host al 8501 del contenedor, que es donde corre streamlit
 -p 8080:8501 \
-# Hay que asegurarse de que exista el volumen bo-container
 -v bo-volume:/app/data \
-# Hay que asegurarse que exista la imagen back-order-app:v1.0
-back-order-app:v1.0
+bo-image:latest
