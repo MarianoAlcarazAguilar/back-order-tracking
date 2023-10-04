aa1a69b82f450123453d9fc8abda2ff744876c39
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-10-04 13:34:27 -0600

    refactor data

--- 1_📤_Cargar_Archivos.py
+++ 1_📤_Cargar_Archivos.py
@@ -2,40 +2,40 @@ from scripts.file_cleaner import FileCleaner
 from scripts.file_manager import FileManager
 import streamlit as st
 
-def update_inventarios(fc: FileCleaner, fm: FileManager):
+def update_inventarios(fc: FileCleaner, fm: FileManager, data_dir:str):
     st.title('Limpieza de Inventarios')
     uploaded_files = st.file_uploader('Actualización de inventarios', type=['xls', 'xlsx'], accept_multiple_files=True)
     
     if len(uploaded_files) > 0:
         for file in uploaded_files:
             try:
-                fc.clean_inventory_file(file, 'transformed_data/inventarios')
-                fm.save_file(file, 'raw_data/inventarios')
+                fc.clean_inventory_file(file, f'{data_dir}/transformed_data/inventarios')
+                fm.save_file(file, f'{data_dir}/raw_data/inventarios')
                 st.success(f'{file.name} se cargó exitosamente')
             except:
                 # Llamamos también a la función de limpieza de transferencias
                 try:
-                    fc.clean_transferencias(file, 'transformed_data/transferencias')
-                    fm.save_file(file, 'raw_data/transferencias')
+                    fc.clean_transferencias(file, f'{data_dir}/transformed_data/transferencias')
+                    fm.save_file(file, f'{data_dir}/raw_data/transferencias')
                     st.success(f'{file.name} se cargó exitosamente')
                 except:
                     st.error(f'Error al cargar {file.name}')
 
 
-def update_forecasts(fc: FileCleaner, fm:FileManager):
+def update_forecasts(fc: FileCleaner, fm:FileManager, data_dir:str):
     st.title('Limpieza de Forecasts')
     uploaded_files = st.file_uploader('Actualización de forecasts', type=['xls', 'xlsx'], accept_multiple_files=True)
 
     if len(uploaded_files) > 0:
         for file in uploaded_files:
             try:
-                fc.clean_forecast_file(file, 'transformed_data/forecasts')
-                fm.save_file(file, 'raw_data/forecasts')
+                fc.clean_forecast_file(file, f'{data_dir}/transformed_data/forecasts')
+                fm.save_file(file, f'{data_dir}/raw_data/forecasts')
                 st.success(f'{file.name} se cargó exitosamente')
             except:
                 st.error(f'Error al cargar {file.name}')
 
-def update_transferencias(fc: FileCleaner, fm: FileManager):
+def update_transferencias(fc: FileCleaner, fm: FileManager, data_dir:str):
     st.title('Limpieza de Transferencias')
     uploaded_files = st.file_uploader('Actualización de transferencias', type=['xls', 'xlsx'], accept_multiple_files=True)
 
@@ -43,16 +43,16 @@ def update_transferencias(fc: FileCleaner, fm: FileManager):
         for file in uploaded_files:
             try:
                 # Llamamos a la función de limpieza
-                fc.clean_transferencias(file, 'transformed_data/transferencias')
+                fc.clean_transferencias(file, f'{data_dir}/transformed_data/transferencias')
                 # Guardamos el archivo de forma local
-                fm.save_file(file, 'raw_data/transferencias')
+                fm.save_file(file, f'{data_dir}/raw_data/transferencias')
                 # Imrpimimos mensaje de success
                 st.success(f'{file.name} se cargó exitosamente')
             except:
                 try:
                     # Intentamos también con el inventario
-                    fc.clean_inventory_file(file, 'transformed_data/inventarios')
-                    fm.save_file(file, 'raw_data/inventarios')
+                    fc.clean_inventory_file(file, f'{data_dir}/transformed_data/inventarios')
+                    fm.save_file(file, f'{data_dir}/raw_data/inventarios')
                     st.success(f'{file.name} se cargó exitosamente')
                 except:
                     st.error(f'Error al cargar {file.name}')
@@ -60,6 +60,7 @@ def update_transferencias(fc: FileCleaner, fm: FileManager):
 
 
 def mariano_app():
+    data_dir = '.'
     st.set_page_config(
         layout='wide', 
         initial_sidebar_state='expanded',
@@ -70,7 +71,7 @@ def mariano_app():
     fc = FileCleaner()
     fm = FileManager()
 
-    with open('static_data/style.css') as f:
+    with open(f'{data_dir}/static_data/style.css') as f:
         # Cargamos el estilo de css (estoy utilizando uno de internet)
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
@@ -83,13 +84,13 @@ def mariano_app():
     type_of_file = st.sidebar.radio('Elige el tipo de archivo a subir', ['Inventario', 'Forecast', 'Transferencias'])
 
     if type_of_file == 'Inventario':
-        update_inventarios(fc, fm)
+        update_inventarios(fc, fm, data_dir=data_dir)
 
     elif type_of_file == 'Forecast':
-        update_forecasts(fc, fm)
+        update_forecasts(fc, fm, data_dir=data_dir)
 
     elif type_of_file == 'Transferencias':
-        update_transferencias(fc, fm)
+        update_transferencias(fc, fm, data_dir=data_dir)
 
 
 if __name__ == '__main__':
